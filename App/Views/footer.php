ca0ea918ba09a1c7464a7065e374a00d2500040a
paulolacerdasilva <plslacerda@gmail.com>
2024-09-11 10:10:43 -0400

    alinhamento centralizado do texto footer

--- App/Views/footer.php
+++ App/Views/footer.php
@@ -1,5 +1,5 @@
 <footer class="bg-dark p-5 text-light">
-  <div class="container">
+  <div class="container text-center">
   <small> Aula de PHP Orientado a Objetos e MVC. Versão <?= APP_VERSAO ?>
      <div class="border-top mt-3">
       &COPY; 2024 - <?= date('Y') ?> IFRO-Guajará-Mirim / Informática - Programação Web
